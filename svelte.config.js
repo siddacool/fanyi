7b5c6d49b58b7aaffce1416aca5658b9eca62ade
Siddhesh Mangela <sidthecool007@gmail.com>
2025-10-06 14:35:33 +0530

    possible fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://svelte.dev/docs/kit/integrations
@@ -8,10 +10,13 @@ const config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({
-      fallback: '404.html',
+      // default options are fine, can also set pages and assets folders
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
     }),
     paths: {
-      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+      base: dev ? '' : '/fanyi', // your repo name
     },
   },
 };
